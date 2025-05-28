9a6847ed1fd1525003b8ca940a60f2e448235da3
Evandro Jardini <eajardini@yahoo.com.br>
2025-05-28 08:02:43 -0300

    Update 54_[Python]_Vetor_Comando03.py

--- 54_[Python]_Vetor_Comando03.py
+++ 54_[Python]_Vetor_Comando03.py
@@ -1,5 +1,6 @@
 '''
-faça um algoritmo que solicite ao usuário números e os armazene em um vetor de 6 posições. O usuário deverá inserir valores positivos e negativos. Substitua todas as ocorrência de valores positivos por 1 e todos os valores negativos por 0.
+Faça um algoritmo que solicite ao usuário números e os armazene em um vetor de 6 posições. O usuário deverá inserir valores positivos e negativos. 
+Substitua todas as ocorrência de valores positivos por 1 e todos os valores negativos por 0.
 
 '''
 
